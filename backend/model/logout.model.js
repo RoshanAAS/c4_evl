39903d9c499d3c4e26dc132d5ad8bd324fb94e69
Roshan Kr Routh <roshan555routh@gmail.com>
2023-06-12 16:39:00 +0530

    new push

---
+++ backend/model/logout.model.js
@@ -0,0 +1,13 @@
+
+
+const mongoose = require("mongoose");
+
+const logoutSchema = mongoose.Schema({
+      token:String
+},{
+    versionKey:false
+});
+
+const LogoutModel=mongoose.model("logout",logoutSchema)
+
+module.exports={LogoutModel}
